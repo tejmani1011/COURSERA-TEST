9dbbfb7a9379fe81c42bbf119b9a1b0f3a417ddb
tejmani1011 <tejmani1011@gmail.com>
2024-08-14 19:54:35 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,7 +3,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for(var i =0; i<names.length; i++)
 {
         if(names[i].charAt(0)==='j' || names[i].charAt(0)==='J')
-        	     console.log("Goodbye "+names[i]);
+        	     console.log("Good Bye "+names[i]);
         else
         	    console.log("Hello "+names[i]);
 }
